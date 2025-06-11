47cd4532e83ac815cbd127b20d248787399c8456
Azmi <azmiozmen5234@gmail.com>
2025-06-11 17:32:40 +0300

    update

---
+++ Booking.RapidAPI/ViewComponents/Default/_DefaultBookingComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Booking.RapidAPI.ViewComponents.Default
+{
+    public class _DefaultBookingComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Booking.RapidAPI/ViewComponents/Default/_DefaultRoomComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class _DefaultRoomComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+     
+
+        return View();
+    }
+}
